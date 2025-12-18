4a107df7ff13db7a0e4c8fd9c75abbdb8809bcd8
Het Gokani <het.parshtechnologies@gmail.com>
2025-12-18 12:45:24 +0530

    Clean submission for 2.7

---
+++ app/pingpong/src/main.rs
@@ -0,0 +1,81 @@
+use axum::{routing::get, Extension, Router};
+use sqlx::Row;
+use std::env;
+use std::error::Error;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+struct Pings {
+    pub id: i32,
+    pub pongs: i32,
+}
+
+async fn read(conn: &sqlx::PgPool) -> Result<Pings, Box<dyn Error>> {
+    let q = "SELECT * FROM pings WHERE id = 1";
+    let query = sqlx::query(q);
+
+    let row = query.fetch_one(conn).await.unwrap();
+    let pings = Pings {
+        id: row.get("id"),
+        pongs: row.get("pongs"),
+    };
+
+    Ok(pings)
+}
+
+async fn write(pongs: &i32, conn: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    let query = "UPDATE pings SET pongs = $1 WHERE id = 1";
+
+    sqlx::query(query).bind(&pongs).execute(conn).await.unwrap();
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let url: String = match env::var("DB_URL") {
+        Ok(val) => val,
+        Err(_e) => String::from("Environment variable DB_URL is not defined."),
+    };
+
+    let pool = sqlx::postgres::PgPool::connect(&url).await.unwrap();
+
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+
+    let address: String = String::from("0.0.0.0:3033");
+
+    let router = Router::new()
+        .route("/pingpong", get(index))
+        .into_make_service();
+
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+
+    println!("Server started on port {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn index() -> String {
+    let url: String = match env::var("DB_URL") {
+        Ok(val) => val,
+        Err(_e) => String::from("Environment variable DB_URL is not defined."),
+    };
+    let pool = sqlx::postgres::PgPool::connect(&url).await.unwrap();
+
+    let mut pongs = match read(&pool).await {
+        Ok(data) => data.pongs,
+        Err(_) => -1,
+    };
+    println!("Retrieved {} pongs from the DB", pongs);
+
+    pongs += 1;
+
+    // write to the DB
+    if let Err(e) = write(&pongs, &pool).await {
+        eprintln!("Failed to write to DB: {}", e);
+        return format!("Error: {}", e);
+    }
+
+    // output to user
+    format!("Ping / Pongs: {}", pongs)
+}
