a6e7d5b92f522b7a3b2f64a493c2e1e79c1a7afa
Het Gokani <het.parshtechnologies@gmail.com>
2025-12-18 12:48:45 +0530

    Clean submission for 5.7

---
+++ app/pingpong/src/main.rs
@@ -0,0 +1,115 @@
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use sqlx::Row;
+use std::env;
+use std::error::Error;
+
+struct Pings {
+    pub id: i32,
+    pub pongs: i32,
+}
+
+async fn read(conn: &sqlx::PgPool) -> Result<Pings, Box<dyn Error>> {
+    let q = "SELECT * FROM pings WHERE id = 1";
+    let query = sqlx::query(q);
+
+    let row = query.fetch_one(conn).await.unwrap();
+    let pings = Pings {
+        id: row.get("id"),
+        pongs: row.get("pongs"),
+    };
+
+    Ok(pings)
+}
+
+async fn write(pongs: &i32, conn: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    let query = "UPDATE pings SET pongs = $1 WHERE id = 1";
+
+    sqlx::query(query).bind(&pongs).execute(conn).await.unwrap();
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let url: String = match env::var("DB_URL") {
+        Ok(val) => val,
+        Err(_e) => String::from("Environment variable DB_URL is not defined."),
+    };
+
+    let pool = sqlx::postgres::PgPool::connect(&url).await.unwrap();
+
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+
+    let mut address: String = String::from("0.0.0.0:3033");
+
+    match env::var("PING_PORT") {
+        Ok(val) => {
+            address = address.replace("3033", &val);
+        }
+        Err(_e) => println!("Environment variable PORT not defined. Using default port 3033"),
+    }
+
+    let router = Router::new()
+        .route("/", get(all_ok))
+        .route("/pingpong", get(index))
+        .route("/healthz", get(healthcheck))
+        .into_make_service();
+
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+
+    println!("Server started on port {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn index() -> String {
+    let url: String = match env::var("DB_URL") {
+        Ok(val) => val,
+        Err(_e) => String::from("Environment variable DB_URL is not defined."),
+    };
+
+    let pool = sqlx::postgres::PgPool::connect(&url).await.unwrap();
+
+    let mut pongs = match read(&pool).await {
+        Ok(data) => data.pongs,
+        Err(_) => -1,
+    };
+    println!("Retrieved {} pongs from the DB", pongs);
+
+    pongs += 1;
+
+    // write to the DB
+    if let Err(e) = write(&pongs, &pool).await {
+        eprintln!("Failed to write to DB: {}", e);
+        return format!("Error: {}", e);
+    }
+
+    // output to user
+    format!("Ping / Pongs: {}", pongs)
+}
+
+async fn all_ok() -> impl IntoResponse {
+    println!("Received a diagnostic request");
+
+    (StatusCode::OK, "All is OK").into_response()
+}
+
+async fn healthcheck() -> impl IntoResponse {
+    println!("Received a healthz request");
+
+    let url: String = match env::var("DB_URL") {
+        Ok(val) => val,
+        Err(_e) => String::from("Environment variable DB_URL is not defined."),
+    };
+
+    match sqlx::postgres::PgPool::connect(&url).await {
+        Ok(_data) => {
+            println!("DB connection established");
+            (StatusCode::OK, "DB connection established").into_response()
+        }
+        Err(_) => {
+            println!("DB unaccessible");
+            (StatusCode::from_u16(500).unwrap(), "DB unaccessible").into_response()
+        }
+    }
+}
